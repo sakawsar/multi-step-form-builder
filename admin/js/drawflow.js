d07b154df33f99139d490c8a0b5823a298788013
unknown <sakawsar70@gmail.com>
2023-07-26 11:41:16 +0600

    Found the remove node method

--- admin/js/drawflow.js
+++ admin/js/drawflow.js
@@ -5,47 +5,60 @@ if(id != null){
     editor.reroute = true;
     const dataToImport = {"drawflow":{"Home":{"data":{"1":{"id":1,"name":"welcome","data":{},"class":"welcome","html":"\n    <div>\n      <div class=\"title-box\">👏 Welcome!!</div>\n      <div class=\"box\">\n        <p>Simple flow library <b>demo</b>\n        <a href=\"https://github.com/jerosoler/Drawflow\" target=\"_blank\">Drawflow</a> by <b>Jero Soler</b></p><br>\n\n        <p>Multiple input / outputs<br>\n           Data sync nodes<br>\n           Import / export<br>\n           Modules support<br>\n           Simple use<br>\n           Type: Fixed or Edit<br>\n           Events: view console<br>\n           Pure Javascript<br>\n        </p>\n        <br>\n        <p><b><u>Shortkeys:</u></b></p>\n        <p>🎹 <b>Delete</b> for remove selected<br>\n        💠 Mouse Left Click == Move<br>\n        ❌ Mouse Right == Delete Option<br>\n        🔍 Ctrl + Wheel == Zoom<br>\n        📱 Mobile support<br>\n        ...</p>\n      </div>\n    </div>\n    ","typenode": false, "inputs":{},"outputs":{},"pos_x":50,"pos_y":50},"2":{"id":2,"name":"slack","data":{},"class":"slack","html":"\n          <div>\n            <div class=\"title-box\"><i class=\"fab fa-slack\"></i> Slack chat message</div>\n          </div>\n          ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"7","input":"output_1"}]}},"outputs":{},"pos_x":1028,"pos_y":87},"3":{"id":3,"name":"telegram","data":{"channel":"channel_2"},"class":"telegram","html":"\n          <div>\n            <div class=\"title-box\"><i class=\"fab fa-telegram-plane\"></i> Telegram bot</div>\n            <div class=\"box\">\n              <p>Send to telegram</p>\n              <p>select channel</p>\n              <select df-channel>\n                <option value=\"channel_1\">Channel 1</option>\n                <option value=\"channel_2\">Channel 2</option>\n                <option value=\"channel_3\">Channel 3</option>\n                <option value=\"channel_4\">Channel 4</option>\n              </select>\n            </div>\n          </div>\n          ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"7","input":"output_1"}]}},"outputs":{},"pos_x":1032,"pos_y":184},"4":{"id":4,"name":"email","data":{},"class":"email","html":"\n            <div>\n              <div class=\"title-box\"><i class=\"fas fa-at\"></i> Send Email </div>\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"5","input":"output_1"}]}},"outputs":{},"pos_x":1033,"pos_y":439},"5":{"id":5,"name":"template","data":{"template":"Write your template"},"class":"template","html":"\n            <div>\n              <div class=\"title-box\"><i class=\"fas fa-code\"></i> Template</div>\n              <div class=\"box\">\n                Ger Vars\n                <textarea df-template></textarea>\n                Output template with vars\n              </div>\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"6","input":"output_1"}]}},"outputs":{"output_1":{"connections":[{"node":"4","output":"input_1"},{"node":"11","output":"input_1"}]}},"pos_x":607,"pos_y":304},"6":{"id":6,"name":"github","data":{"name":"https://github.com/jerosoler/Drawflow"},"class":"github","html":"\n          <div>\n            <div class=\"title-box\"><i class=\"fab fa-github \"></i> Github Stars</div>\n            <div class=\"box\">\n              <p>Enter repository url</p>\n            <input type=\"text\" df-name>\n            </div>\n          </div>\n          ","typenode": false, "inputs":{},"outputs":{"output_1":{"connections":[{"node":"5","output":"input_1"}]}},"pos_x":341,"pos_y":191},"7":{"id":7,"name":"facebook","data":{},"class":"facebook","html":"\n        <div>\n          <div class=\"title-box\"><i class=\"fab fa-facebook\"></i> Facebook Message</div>\n        </div>\n        ","typenode": false, "inputs":{},"outputs":{"output_1":{"connections":[{"node":"2","output":"input_1"},{"node":"3","output":"input_1"},{"node":"11","output":"input_1"}]}},"pos_x":347,"pos_y":87},"11":{"id":11,"name":"log","data":{},"class":"log","html":"\n            <div>\n              <div class=\"title-box\"><i class=\"fas fa-file-signature\"></i> Save log file </div>\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"5","input":"output_1"},{"node":"7","input":"output_1"}]}},"outputs":{},"pos_x":1031,"pos_y":363}}},"Other":{"data":{"8":{"id":8,"name":"personalized","data":{},"class":"personalized","html":"\n            <div>\n              Personalized\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"12","input":"output_1"},{"node":"12","input":"output_2"},{"node":"12","input":"output_3"},{"node":"12","input":"output_4"}]}},"outputs":{"output_1":{"connections":[{"node":"9","output":"input_1"}]}},"pos_x":764,"pos_y":227},"9":{"id":9,"name":"dbclick","data":{"name":"Hello World!!"},"class":"dbclick","html":"\n            <div>\n            <div class=\"title-box\"><i class=\"fas fa-mouse\"></i> Db Click</div>\n              <div class=\"box dbclickbox\" ondblclick=\"showpopup(event)\">\n                Db Click here\n                <div class=\"modal\" style=\"display:none\">\n                  <div class=\"modal-content\">\n                    <span class=\"close\" onclick=\"closemodal(event)\">&times;</span>\n                    Change your variable {name} !\n                    <input type=\"text\" df-name>\n                  </div>\n\n                </div>\n              </div>\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[{"node":"8","input":"output_1"}]}},"outputs":{"output_1":{"connections":[{"node":"12","output":"input_2"}]}},"pos_x":209,"pos_y":38},"12":{"id":12,"name":"multiple","data":{},"class":"multiple","html":"\n            <div>\n              <div class=\"box\">\n                Multiple!\n              </div>\n            </div>\n            ","typenode": false, "inputs":{"input_1":{"connections":[]},"input_2":{"connections":[{"node":"9","input":"output_1"}]},"input_3":{"connections":[]}},"outputs":{"output_1":{"connections":[{"node":"8","output":"input_1"}]},"output_2":{"connections":[{"node":"8","output":"input_1"}]},"output_3":{"connections":[{"node":"8","output":"input_1"}]},"output_4":{"connections":[{"node":"8","output":"input_1"}]}},"pos_x":179,"pos_y":272}}}}}
     editor.start();
+    // modified import data
+    let mod_import_data = { ...msfb.raw_data }
     if( msfb.raw_data ) {
+      let node_data = []
       // update the html DOM
       for( the_node_key in msfb.raw_data.drawflow.Home.data ) {
+        // continue
         // node to be updated
         let the_node = msfb.raw_data.drawflow.Home.data[the_node_key]
         // node name
         let node_name = the_node.name
+        // node id
+        let node_id = the_node.id
         // the updated html for the node
         let updated_html = update_the_nodes_with_raw_data(node_name)
         // previous html of the node
-        let prev_html = msfb.raw_data.drawflow.Home.data[the_node_key].html
+        let prev_html = msfb.raw_data.drawflow.Home.data[the_node_key].html // get the previous node dom
         // previous options of the node
         let prev_options = jQuery(prev_html).find('ul li').length
         // current options of the node
         let current_options = jQuery(updated_html).find('ul li').length
         console.log('current options',current_options, prev_options)
+        console.log('node_name',node_name)
         // update the html 
-        msfb.raw_data.drawflow.Home.data[the_node_key].html = updated_html
+        msfb.raw_data.drawflow.Home.data[the_node_key].html = updated_html // update the previous node dom
         // update the outputs
+        node_data.push({ node_name, node_id, prev_options, current_options })
+      }
+      editor.import(msfb.raw_data);
+      for (let i = 0; i < node_data.length; i++) {
+        let node_name = node_data[i].node_name
+        let prev_options = node_data[i].prev_options
+        let current_options = node_data[i].current_options
+        let node_id = node_data[i].node_id
         let qtn_type = jQuery('div[data-node="'+node_name+'"]').attr('data-question-type')
         if ( qtn_type == "msfb-single-select-field" || qtn_type == "msfb-dropdown-field" ) {
-          let new_options = []
-          let new_input_options = []
           if( prev_options !== current_options ) {
-            let inputs = msfb.raw_data.drawflow.Home.data[the_node_key].outputs
-            let outputs = msfb.raw_data.drawflow.Home.data[the_node_key].outputs
             for( let i = 1; i <= current_options; i++ ) {
-              let this_opt_key = `output_${i}`
-              let this_int_key = `input_${i}`
-              new_options[this_opt_key] = outputs[this_opt_key] ? outputs[this_opt_key] : {connections:[]}
-              new_input_options[this_opt_key] = inputs[this_int_key] ? inputs[this_int_key] : {connections:[]}
+              if( prev_options > current_options ) {
+                  let offset = prev_options - current_options
+                  for( let j = 1; j <= offset; j++ ) {
+                    let this_opt_key = `output_${prev_options - j}`
+                    editor.removeNodeOutput(node_id,this_opt_key)
+                  }
+              } else if( prev_options > current_options ) {
+                let offset = current_options - prev_options
+                for( let j = 1; j <= offset; j++ ) {
+                  editor.addNodeOutput(node_id)
+                }
+              }
             }
-            // Object.assign(outputs, 'output_' + this_opt_key, {connections:[]})
-            console.log({...new_options})
-            msfb.raw_data.drawflow.Home.data[the_node_key].outputs = {...new_options}
-            msfb.raw_data.drawflow.Home.data[the_node_key].inputs = {...new_input_options}
           }
         }
       }
-      console.log('the raw data',msfb.raw_data.drawflow)
-      editor.import(msfb.raw_data);
     }
     editor.zoom_max = 1.5
     editor.zoom_min = 0.5
