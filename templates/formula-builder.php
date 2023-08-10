888ad8e2fa857c60f5b62280171bcff792fda131
Sarwar A Kawsr <sakawsar70@gmail.com>
2023-08-10 17:03:26 +0600

    Scroll added to the toolbar of the Q-Flow builder

--- templates/formula-builder.php
+++ templates/formula-builder.php
@@ -45,7 +45,7 @@
     <div class="skfb-content">
       <div class="sk-container">
         <div class="sk-row">
-          <div class="sk-col-md-3 sk-col-lg-2 skfb__overflow_scroll">
+          <div class="sk-col-md-3 sk-col-lg-2 skfb__overflow_scroll" style="height:600px;">
             <div class="skfb-feilds-panel">
             <div class="msfb-filter-tab">
                     <div class="msfb-qtn-tab msfb-tab-selected" data-type="qtn">
